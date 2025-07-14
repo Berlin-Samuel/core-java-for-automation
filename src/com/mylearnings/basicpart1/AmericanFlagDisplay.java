fa45cfc21a5c63a4e5207519db8be62a6d5c427a
Berlin-Samuel <rberlinsamuel@gmail.com>
2025-07-14 23:40:56 +0530

    change made

--- src/com/mylearnings/basicpart1/AmericanFlagDisplay.java
+++ src/com/mylearnings/basicpart1/AmericanFlagDisplay.java
@@ -5,7 +5,6 @@ package com.mylearnings.basicpart1;
 public class AmericanFlagDisplay {
 	public static void main(String[] args) {
 		System.out.println("* * * * * * ==================================");
-		
 		System.out.println(" * * * * *  ==================================");
 		System.out.println("* * * * * * ==================================");
 		System.out.println(" * * * * *  ==================================");
