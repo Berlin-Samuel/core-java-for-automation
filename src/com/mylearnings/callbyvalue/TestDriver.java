fbfb5e26ca3bdf819ab2839184db8eac78e9ac4a
Berlin-Samuel <rberlinsamuel@gmail.com>
2025-11-10 14:50:06 +0530

    method and its types covered

---
+++ src/com/mylearnings/callbyvalue/TestDriver.java
@@ -0,0 +1,44 @@
+/**
+ * FileName: TestDriver.java
+ * Author   : Admin
+ * Version  : 1.0
+ * Date     : 10-Nov-2025
+ * 
+ * This file is part of a personal learning project.
+ * 
+ * Licensed under the MIT License.
+ * 
+ * Copyright (c) 2025 Berlin Samuel.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  
+ * of this software and associated documentation files (the "Software"), to deal  
+ * in the Software without restriction, including without limitation the rights  
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
+ * copies of the Software, and to permit persons to whom the Software is  
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in  
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * 
+ * REVISION     DATE           NAME         DESCRIPTION  
+ * 1.0          10-Nov-2025        Berlin        Initial Code  
+ * 
+ * @author Berlin
+ * @version 1.0
+ * @since 10-Nov-2025
+ */
+package com.mylearnings.callbyvalue;
+
+public class TestDriver {
+	public static void main(String[] args) { 
+		int number = 200;
+		System.out.println("Before calling method m1() " + number);
+		Test t1 = new Test();
+		t1.m1(number);
+		System.out.println("After calling method m1() " + number);
+
+	}
+}
