3bedb9cfedf3772c9ec91c17b4c1e5826f43bdff
Berlin-Samuel <rberlinsamuel@gmail.com>
2025-11-21 20:36:57 +0530

    multiple inheritence completed

---
+++ src/com/mylearnings/abstraction/FarmHouse.java
@@ -0,0 +1,47 @@
+/**
+ * FileName: FarmHouse.java
+ * Author   : Admin
+ * Version  : 1.0
+ * Date     : 21-Nov-2025
+ * 
+ * This file is part of a personal learning project.
+ * 
+ * Licensed under the MIT License.
+ * 
+ * Copyright (c) 2025 Berlin Samuel.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  
+ * of this software and associated documentation files (the "Software"), to deal  
+ * in the Software without restriction, including without limitation the rights  
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
+ * copies of the Software, and to permit persons to whom the Software is  
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in  
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * 
+ * REVISION     DATE           NAME         DESCRIPTION  
+ * 1.0          21-Nov-2025        Berlin        Initial Code  
+ * 
+ * @author Berlin
+ * @version 1.0
+ * @since 21-Nov-2025
+ */
+package com.mylearnings.abstraction;
+
+public class FarmHouse implements  Parent1, Parent2 {
+	public void farmHouse() {
+		System.out.println("It is a Farm House");
+	}
+	public void m1() {
+		System.out.println("It is a Parent1 Interface method m1()");
+	}
+
+	public void m2() {
+		System.out.println("It is a Parent2 Interface method m2()");
+
+	}
+}
---
+++ src/com/mylearnings/abstraction/PartnershipLandDriver.java
@@ -0,0 +1,43 @@
+/**
+ * FileName: PartnershipLandDriver.java
+ * Author   : Admin
+ * Version  : 1.0
+ * Date     : 21-Nov-2025
+ * 
+ * This file is part of a personal learning project.
+ * 
+ * Licensed under the MIT License.
+ * 
+ * Copyright (c) 2025 Berlin Samuel.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  
+ * of this software and associated documentation files (the "Software"), to deal  
+ * in the Software without restriction, including without limitation the rights  
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
+ * copies of the Software, and to permit persons to whom the Software is  
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in  
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * 
+ * REVISION     DATE           NAME         DESCRIPTION  
+ * 1.0          21-Nov-2025        Berlin        Initial Code  
+ * 
+ * @author Berlin
+ * @version 1.0
+ * @since 21-Nov-2025
+ */
+package com.mylearnings.abstraction;
+
+public class PartnershipLandDriver {
+	public static void main(String[] args) {
+		PartnershipLand ch1 = new PartnershipLand();
+		ch1.m1();
+		ch1.m2();
+		ch1.farmHouse();
+		System.out.println(Parent1.x + Parent2.y);
+	}
+}
